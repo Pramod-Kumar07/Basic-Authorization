a05bd36d7ddc0873658cff8207e8f7218de0b2c2
Pramod <123134499+Pramod-Kumar07@users.noreply.github.com>
2026-08-22 01:54:39 +0530

    feat/cors fix

--- src/server.js
+++ src/server.js
@@ -10,7 +10,12 @@ require("dotenv").config();
 const app = express();
 
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
